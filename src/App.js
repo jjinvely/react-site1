ab708a2a615a80cf45ae8ddaf7d9dbdfc4b859c6
yeojin Kim <jjiny2022@kakao.com>
2022-07-26 00:09:57 +0900

    22/07/26
    react 자바스크립트 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Slider from './components/Slider';
-import ImgText from './components/ImgText';
-import Image from './components/Image';
-import Card from './components/Card';
-import Banner from './components/Banner';
-import Text from './components/Text';
-import Footer from './components/Footer';
-import Contents from './components/Contents';
+import Slider from './components/pages/Slider';
+import ImgText from './components/pages/ImgText';
+import Image from './components/pages/Image';
+import Card from './components/pages/Card';
+import Banner from './components/pages/Banner';
+import Text from './components/pages/Text';
+
 
 //자바스크립트 : 동적인 부분(데이터)
 //데이터 입력, 출력(데이터 저정하기, 데이터 불러오기)
@@ -19,16 +16,15 @@ import Contents from './components/Contents';
 function App() {
   return (
     <>
-      <Header fonts="nexon" />
-      <Contents>
-        <Slider fonts={['slider__wrap', 'nexon']} />
-        <Image skill={['section', 'nexon']} />
-        <ImgText skill={['section', 'nexon', 'gray']} />
-        <Card skill={['section', 'nexon']} />
-        <Banner skill={['section', 'gmarket']} />
-        <Text skill={['section', 'nexon']} />
-      </Contents>
-      <Footer skill={['section', 'nexon', 'gray']} />
+     
+        <Slider/>
+        <Image/>
+        <ImgText/>
+        <Card/>
+        <Banner/>
+        <Text/>
+     
+      
     </>
   );
 }
